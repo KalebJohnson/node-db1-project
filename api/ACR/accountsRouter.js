5abef1c2ce13524b110d5985853bf6cbf6dc04f0
unknown <KalebJohnson45@yahoo.com>
2020-08-20 07:33:00 -0500

    mvp, checking endpoints

---
+++ api/ACR/accountsRouter.js
@@ -0,0 +1,93 @@
+const express = require("express")
+const db = require("../../data/dbConfig")
+
+const router = express.Router()
+
+//GET ACCOUNTS
+router.get("/", async (req, res, next) => {
+	try {
+		const accounts = await db.select("*").from("accounts")
+		res.json(accounts)
+	} catch (err) {
+		next(err)
+	}
+})
+
+//GET BY :ID
+router.get("/:id", async (req, res, next) => {
+	try {	
+        const account = await db("accounts")
+            .where("id", req.params.id)
+            .first()
+
+        res.json(account)
+	} catch (err) {
+		next(err)
+	}
+})
+
+//POST
+router.post("/",validateAcc, async (req, res, next) => {
+	try {
+		const input = {
+			name: req.body.name,
+			budget: req.body.budget,
+		}
+		const [ID] = await db.insert(input).into("accounts")
+		
+		const account = await db.first("*").from("accounts").where("id", ID)
+
+		res.status(201).json(account)
+	} catch (err) {
+		next(err)
+	}
+})
+
+//PUT-EDIT
+router.put("/:id",validateAcc, async (req, res, next) => {
+	try {
+		const input = {
+			name: req.body.name,
+			budget: req.body.budget,
+		}
+		if (req.body.name || req.body.budget) {
+			await db("accounts")
+			.update(input)
+			.where("id", req.params.id)
+		}
+		const account = await db.first("*")
+			.from("accounts")
+			.where("id", req.params.id)
+
+		res.json(account)
+	} catch (err) {
+		next(err)
+	}
+})
+
+//DELETE
+router.delete("/:id", async (req, res, next) => {
+	try {
+		await db("accounts").where("id", req.params.id).del()
+		res.status(204).end()
+	} catch (err) {
+		next(err)
+	}
+})
+
+//CMD
+function validateAcc(req, res, next) {
+	if (Object.keys(req.body).length !== 0) {
+	  (req.body.budget)
+		? next()
+		: res.status(400).json({
+		  error: "budget REQUIRED"
+		})
+	} else {
+	  res.status(400).json({
+		error: "No account data found"
+	  })
+	}
+  }
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const server = require("./api/server.js");
+const accountsRouter = require("./api/ACR/accountsRouter")
+const express = require("express")
+
+server.use(express.json())
+
+server.use("/accounts", accountsRouter)
+
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
 });
+
